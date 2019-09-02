21df5798a316789447f0c8dfb0e90204c4ef4ce5
zhaofa <zhaofa@huapubj.com>
2019-09-02 13:40:31 +0800

    feat(huafenerc):蒙层界面相关

--- huafener/TabbarVC/LYTabbarViewController.m
+++ huafener/TabbarVC/LYTabbarViewController.m
@@ -9,7 +9,12 @@
 #import "LYTabbarViewController.h"
 #import "UITabBarController+LYTabbarSetting.h"
 
+#import "ZZCustomAlertView.h"
+#import "HPCenterBtnView.h"
+
 @interface LYTabbarViewController ()
+
+@property (nonatomic, strong) ZZCustomAlertView   *alertView;
 
 @end
 
@@ -18,7 +23,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setUIBeheaves];
+    NSLog(@"%ld",self.selectedIndex);
 }
 
+- (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item{
+    if ([item.title isEqualToString:@"发布"]) {
+        
+        HPCenterBtnView *btnView_ = [[HPCenterBtnView alloc] initWithFrame:[UIScreen mainScreen].bounds campaigns:@[]];
+        
+        _alertView = [ZZCustomAlertView alertViewWithParentView:[UIApplication sharedApplication].keyWindow andContentView:btnView_];
+        _alertView.shouldBlurBackground = YES;
+        _alertView.allowTapBackgroundToDismiss = NO;
+        _alertView.shadowColor = [UIColor whiteColor];
+        _alertView.shadowAlpha = 0.60f;
+        [_alertView show];
+
+        WS(weakSelf);
+        btnView_.touchSelfViewBlock = ^{
+            [weakSelf.alertView dismissWithCompletionBlock:^(BOOL finished) {
+                weakSelf.alertView = nil;
+            }];
+        };
+        
+    }
+}
 
 @end
