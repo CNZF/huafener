889ef8289c12144d3f2ec16220eb4a0d8c7a0454
zhaofa-hp <zhaofa@huapubj.com>
2020-05-12 13:38:08 +0800

    16进制颜色设置宏定义

--- huafener/HPHeaderDefine/HPDefineColors/HPDefineColors.h
+++ huafener/HPHeaderDefine/HPDefineColors/HPDefineColors.h
@@ -16,3 +16,9 @@
 #define GH_COLOR_FROM_RGBA(rgbValue,a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
 
 #define GH_Color_Random [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1.0]
+
+#define HPTEXTCOLOR_333 GH_COLOR_FROM_RGB (0x333333)
+#define HPTEXTCOLOR_888 GH_COLOR_FROM_RGB (0x888888)
+#define HPTEXTCOLOR_ccc GH_COLOR_FROM_RGB (0xcccccc)
+#define HPTEXTCOLOR_f67 GH_COLOR_FROM_RGB (0xff6677)
+#define HPTEXTCOLOR_line GH_COLOR_FROM_RGB(0xE2E2E2)
